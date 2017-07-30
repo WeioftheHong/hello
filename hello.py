8a2118a8231ae14a4c385145990cca8620973e12
weihong <wei-hong@hotmail.com>
2017-07-30 14:06:56 +1000

    FizzBuzz function added

--- hello.py
+++ hello.py
@@ -1,3 +1,25 @@
 # hello.py
 # says 'Hello, git!'
+# also does fizzBuzz
+
+def fizzBuzzIterative(start, end):
+    for i in range(start, end):
+        fizzBuzzPrint(i)
+
+def fizzBuzzPrint(n):
+    mod3 = not n % 3
+    mod5 = not n % 5
+    if (not mod3 and not mod5):
+        print(n)
+    elif (mod3 and mod5):
+        print("Fizz Buzz")
+    elif (mod3):
+        print("Fizz ")
+    elif (mod5):
+        print("Buzz ")
+
 print("'Hello, git!'")
+
+n = int(input("Fizz Buzz #? "))
+
+fizzBuzzIterative(1, n)
