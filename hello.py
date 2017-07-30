87130b8b2510cb6468cb21f44c31459bffe5c7b2
weihong <wei-hong@hotmail.com>
2017-07-30 17:53:23 +1000

    hello updated fizzbuzz development

--- hello.py
+++ hello.py
@@ -1,5 +1,27 @@
 # hello.py
 # says 'Hello, git!'
+<<<<<<< HEAD
+=======
+# also does fizzBuzz
+# development branch
+#
+
+def fizzBuzzIterative(start, end):
+    for i in range(start, end):
+        fizzBuzzPrint(i)
+
+def fizzBuzzPrint(n):
+    mod3 = not n % 3
+    mod5 = not n % 5
+    if (not mod3 and not mod5):
+        print(n)
+    elif (mod3 and mod5):
+        print("Fizz Buzz")
+    elif (mod3):
+        print("Fizz ")
+    elif (mod5):
+        print("Buzz ")
+>>>>>>> development
 
 print("'Hello, git!'")
 
